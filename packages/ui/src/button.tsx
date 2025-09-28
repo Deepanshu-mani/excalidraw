6247bc7b56e772fc53b7346e9869c1013fbb727a
Deepanshu Mani <deepanshukumar1542004@gmail.com>
2025-09-28 23:39:22 +0530

    nothing much

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,18 +4,18 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children?: ReactNode;
-
+  className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export const Button = ({
   children,
-
+  className,
   onClick,
 }: ButtonProps) => {
   return (
     <button
-      className="px-4 py-2 rounded-2xl border border-white/20 bg-black text-white cursor-pointer hover:bg-white/10 "
+      className={`px-4 py-2 rounded-2xl border border-white/20 bg-black text-white cursor-pointer hover:bg-white/10 ${className || ''}`}
       onClick={onClick}
     >
       {children}
